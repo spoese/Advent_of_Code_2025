34f677cc78a4631324aaa5b0b714e3adb7b8e417
spoese <scott.poese@gmail.com>
2025-12-11 11:02:23 -0500

    Day 7 Complete

--- Day7.R
+++ Day7.R
@@ -1,28 +1,28 @@
 library(tidyverse)
 
 #Sample Data
-data <- c(
-  ".......S.......",
-  "...............",
-  ".......^.......",
-  "...............",
-  "......^.^......",
-  "...............",
-  ".....^.^.^.....",
-  "...............",
-  "....^.^...^....",
-  "...............",
-  "...^.^...^.^...",
-  "...............",
-  "..^...^.....^..",
-  "...............",
-  ".^.^.^.^.^...^.",
-  "..............."
-)
+# data <- c(
+#   ".......S.......",
+#   "...............",
+#   ".......^.......",
+#   "...............",
+#   "......^.^......",
+#   "...............",
+#   ".....^.^.^.....",
+#   "...............",
+#   "....^.^...^....",
+#   "...............",
+#   "...^.^...^.^...",
+#   "...............",
+#   "..^...^.....^..",
+#   "...............",
+#   ".^.^.^.^.^...^.",
+#   "..............."
+# )
 
 #Actual Data
-# data <- read_lines("Day7")
-
+data <- read_lines("Day7")
+tictoc::tic()
 #Part 1
 mat_data <- data |>
   str_split("") |>
@@ -51,9 +51,34 @@ for (i in seq(3, nrow(mat_data) - 1, by = 2)) {
 splits
 
 #Part 2
-position <- which(mat_data == "S", arr.ind = TRUE)
 
-path <- c()
-if (mat_data[position] %in% c("S", "|")) {
-  position <- position + c(1, 0)
-} else if (mat_data[position] == "^") {}
+mat_data[which(mat_data == "^", arr.ind = TRUE)] <- "0"
+mat_data <- rbind(mat_data, rep(".", ncol(mat_data)))
+mat_data[nrow(mat_data), which(mat_data[nrow(mat_data) - 1, ] == "|")] <- "0"
+position <- which(mat_data == "S", arr.ind = TRUE) + c(2, 0)
+mat_data[position] <- as.numeric(mat_data[position]) + 1
+
+for (i in 3:(nrow(mat_data) - 2)) {
+  for (j in 2:(ncol(mat_data) - 1)) {
+    if (!is.na(as.numeric(mat_data[i, j]))) {
+      #Go left
+      left_col <- mat_data[i:nrow(mat_data), j - 1]
+      left_stop <- min(which(!is.na(as.numeric(left_col))))
+      left_index <- i + left_stop - 1
+      mat_data[left_index, j - 1] <- as.numeric(mat_data[left_index, j - 1]) +
+        as.numeric(mat_data[i, j])
+      #Go right
+      right_col <- mat_data[i:nrow(mat_data), j + 1]
+      right_stop <- min(which(!is.na(as.numeric(right_col))))
+      right_index <- i + right_stop - 1
+      mat_data[right_index, j + 1] <- as.numeric(mat_data[right_index, j + 1]) +
+        as.numeric(mat_data[i, j])
+    }
+  }
+}
+mat_data
+format(
+  sum(as.numeric(mat_data[nrow(mat_data), ]), na.rm = TRUE),
+  scientific = FALSE
+)
+tictoc::toc()
